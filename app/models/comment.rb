7569f8eab4be5a0a05410ce878a87c2fb4dd10d9
Ryu Sato <ryu@weseek.co.jp>
2018-07-06 04:07:47 +0900

    add annotation to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id            :integer          not null, primary key
+#  body_md       :string           default(""), not null
+#  body_html     :string           default(""), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  url           :string
+#  created_by_id :integer          not null
+#  lock_version  :integer          default(0), not null
+#
+
 class Comment < ApplicationRecord
 end
