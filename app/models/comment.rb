d35a4f119d4abbdd5ac0f4c39a0d5c65291a021f
Ryu Sato <ryu@weseek.co.jp>
2018-07-06 04:25:58 +0900

    update annotation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,16 @@
 #
 # Table name: comments
 #
-#  id            :integer          not null, primary key
-#  body_md       :string           default(""), not null
-#  body_html     :string           default(""), not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  url           :string
-#  created_by_id :integer          not null
-#  lock_version  :integer          default(0), not null
+#  id               :integer          not null, primary key
+#  body_md          :string           default(""), not null
+#  body_html        :string           default(""), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  url              :string
+#  created_by_id    :integer          not null
+#  stargazers_count :integer          default(0), not null
+#  star             :boolean          default(FALSE), not null
+#  lock_version     :integer          default(0), not null
 #
 
 class Comment < ApplicationRecord
