6f11ef80ae7b4d6ad1b35d57dd2b7ea9c11f6861
Ryu Sato <ryu@weseek.co.jp>
2018-07-08 07:30:34 +0900

    modify style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( admin/active_admin.js admin/active_admin.css )
+Rails.application.config.assets.precompile += %w( admin/active_admin.js admin/active_admin.css
+                                                  admin/admin_bootstrap.js admin/admin_bootstrap.css )
